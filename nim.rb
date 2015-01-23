713bae1017b6c322b459dc581a44b008515ee69d
Francesco Zanitti <fzanitti@gmail.com>
2015-01-23 12:14:23 +0100

    Update nim.rb
    brew complains about SHA1 mismatch.

--- nim.rb
+++ nim.rb
@@ -4,7 +4,7 @@ class Nim < Formula
   homepage "http://nim-lang.org/"
 
   url "http://nim-lang.org/download/nim-0.10.2.zip"
-  sha1 '0a54d6d7f257cdade5bf950d318066959c48a6dc'
+  sha1 '2fdd486704931807d03d25d6846c5faee880d69e'
   version "0.10.2"
 
   def install
